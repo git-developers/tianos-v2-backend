0e0fd47be3b2d27d80d33a775eaf4417c1b61e9a
jbendezu <jbendezu@crackthecode.pe>
2022-02-14 23:24:27 -0500

    login correcto

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,19 +2,65 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 // https://symfony.com/doc/current/security/form_login_setup.html
 
 class SecurityController extends AbstractController
 {
+
+
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/login", name="login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(Request $request, UserRepository $userRepository, UserPasswordEncoderInterface $passwordEncoder)
+    {
+
+        //$all = $request->query->all();
+        $data = json_decode($request->getContent(), true);
+        /*
+        echo "<pre>";
+        echo "aaaaaa";
+        print_r($all);
+        exit;
+        */
+
+        $username = $data["username"];
+        $password = $data["password"];
+
+        $user = $userRepository->loadUserByUsername($username);
+
+        if (!$user) {
+            return $this->json([
+                'status' => 'User does not exist'
+            ]);
+        }
+
+        $isGoodPass = $passwordEncoder->isPasswordValid($user, $password);
+
+        if (!$isGoodPass) {
+            return $this->json([
+                'status' => 'password incorrect'
+            ]);
+        }
+
+        return $this->json(
+            [
+                "status" => "logged",
+                "msg" => "success",
+            ]
+        );
+    }
+
+    /**
+     * @Route("/login33", name="app_login")
+     */
+    public function login33(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
@@ -39,6 +85,10 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
+
+
+
+
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
